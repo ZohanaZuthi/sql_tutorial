9b6362abf74c272553cc95413efc4668c5780d82
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2024-09-03 05:48:17 +0600

    Update 4th.sql

--- 4th.sql
+++ 4th.sql
@@ -1,17 +1,24 @@
--- COMPACT FRAME
-
--- For only precedng the current row can be skipped 
--- in normal form- rows between current flow and 2 following
---  in shhort form- rows 2 folllowing
+-- Window function 4 rules
+-- 1. window function can not be written without select and order by function and ost definately inside select function.
+-- 2. window function can not be written within the another window function
+-- 3. window function can not be used for filtering data
+-- 4. window function can be used with group only if the group by operation is same as the window operation .
+-- for example:
 
 
 SELECT 
-	OrderID,
-	OrderDate,
-	OrderStatus,
+	CustomerID
 	Sales,
-	SUM(Sales) OVER(PARTITION BY OrderStatus ORDER BY OrderDate 
-	ROWS  2 PRECEDING) TotalSales
-from Sales.Orders
+	SUM(Sales) TotalSales,
+	RANK() OVER(ORDER BY CustomerID DESC) RankCustomers
+FROM Sales.Orders
+GROUP BY CustomerID
 
--- it only works for preceding
+
+SELECT 
+	CustomerID
+	Sales,
+	SUM(Sales) TotalSales,
+	RANK() OVER(ORDER BY SUM(Sales) DESC) RankCustomers
+FROM Sales.Orders
+GROUP BY CustomerID
