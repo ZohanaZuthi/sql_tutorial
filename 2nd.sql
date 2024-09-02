7ac1cb917a1a9ad1305651ddbe26df6b383bf08a
Nazifa Fairuz Zuthi <startrack613@gmail.com>
2024-09-03 04:32:37 +0600

    Window Function

--- 2nd.sql
+++ 2nd.sql
@@ -24,4 +24,11 @@ SELECT
 -- there are two part of window function
 -- window function+Over clause[partition clause, orderr clause, frame clause]
 
-
+SELECT
+	OrderID,
+	OrderDate,
+    ProductID,
+	Sales,
+	SUM(Sales) OVER() Total_Sale,
+	SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesByProducts
+ FROM Sales.Orders
